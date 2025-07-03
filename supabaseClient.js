d60c8a213e0f58df9f65a6ef40619451a481f395
mnwrhu <138902883+mnwrhu@users.noreply.github.com>
2025-07-04 00:50:16 +0700

    supabaseClient.js

--- supabaseClient.js
+++
@@ -1,4 +0,0 @@
-// Inisialisasi Supabase client
-const supabaseUrl = 'https://zdwjuwbbhqzoqapvhiwn.supabase.co';
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpkd2p1d2JiaHF6b3FhcHZoaXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NTQxNjEsImV4cCI6MjA2NzEzMDE2MX0.62anUbI3t_5Acd_Zbv57y5f5G59KRC07hwx9ovsf-W0'; // kamu sudah benar
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
